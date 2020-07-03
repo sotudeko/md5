63a1934b67c9cd9478174ff6e23721dd18a90a2a
sotudeko <sotudeko@sonatype.com>
2020-07-03 12:36:25 +0100

    0.23

---
+++ conanfile.py
@@ -0,0 +1,33 @@
+from conans import ConanFile, CMake, tools
+
+
+class Md5Conan(ConanFile):
+    name = "md5"
+    version = "1.0"
+    generators = "cmake"
+    exports_sources = "src/*"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="src")
+        cmake.build()
+
+        # Explicit way:
+        # self.run('cmake %s/hello %s'
+        #          % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+    def requirements(self):
+        self.requires("poco/1.9.3")
+        self.requires("zlib/1.2.8")
+
+    def package(self):
+        self.copy("*.h", dst="include", src="hello")
+        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("md5", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["hello"]
